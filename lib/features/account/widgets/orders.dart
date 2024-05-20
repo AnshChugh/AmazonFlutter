14956b05f8f174b10cfbedd9494a1b4b3356319a
AnshChugh <Anshchugh20@GMAil.com>
2024-05-20 15:37:12 +0530

    implemeted model for order

--- lib/features/account/widgets/orders.dart
+++ lib/features/account/widgets/orders.dart
@@ -1,5 +1,8 @@
 import 'package:amazon_flutter/constants/global_variables.dart';
+import 'package:amazon_flutter/features/account/services/account_services.dart';
 import 'package:amazon_flutter/features/account/widgets/single_product.dart';
+import 'package:amazon_flutter/features/auth/widgets/loader.dart';
+import 'package:amazon_flutter/models/order.dart';
 import 'package:flutter/material.dart';
 
 class Orders extends StatefulWidget {
@@ -10,49 +13,62 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
-  List list = [
-    'https://images.unsplash.com/photo-1682687980115-a37b56ea7271?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1684162440652-107bf47ab89b?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://plus.unsplash.com/premium_photo-1702910931459-6cb01f2f3a93?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  ];
+  List<Order>? orders;
+
+  final accountServices = AccountServices();
+
+  void fetchOrders() async {
+    orders = await accountServices.fetchMyOrders(context: context);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchOrders();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 15),
-              child: const Text(
-                'Your Orders',
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+    return orders == null
+        ? const Loader()
+        : Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: const Text(
+                      'Your Orders',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Text(
+                      'See all',
+                      style: TextStyle(
+                          color: GlobalVariables.selectedNavBarColor,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(right: 15),
-              child: Text(
-                'See all',
-                style: TextStyle(
-                    color: GlobalVariables.selectedNavBarColor,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 18),
-              ),
-            ),
-          ],
-        ),
-        Container(
-            height: 170,
-            padding: const EdgeInsets.only(left: 10, top: 20, right: 0),
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                // temporary list
-                return SingleProduct(img: list[index]);
-              },
-            )),
-      ],
-    );
+              Container(
+                  height: 170,
+                  padding: const EdgeInsets.only(left: 10, top: 20, right: 0),
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: orders!.length,
+                    itemBuilder: (context, index) {
+                      // temporary list
+                      return SingleProduct(
+                          img: orders![index].products[0].images[0]);
+                    },
+                  )),
+            ],
+          );
   }
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -111,5 +111,14 @@ userRouter.post('/api/order', auth, async (req,res)=>{
     }
 });
 
+userRouter.get('/api/my-orders', auth , async (req,res) =>{
+    try{
+        const orders = await Order.find({userId: req.user});
+        res.json(orders);
+    }catch(e){
+        res.status(500).json({error:e.message});
+    }
+});
+
 
 module.exports = {userRouter:userRouter};
