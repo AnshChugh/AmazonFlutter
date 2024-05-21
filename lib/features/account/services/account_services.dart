c7611d1e02b255c8d53e4d50e33c8965a330f9a2
AnshChugh <Anshchugh20@GMAil.com>
2024-05-21 22:26:29 +0530

    added log out feature to user panel

--- lib/features/account/services/account_services.dart
+++ lib/features/account/services/account_services.dart
@@ -3,11 +3,13 @@ import 'dart:convert';
 import 'package:amazon_flutter/constants/error_handling.dart';
 import 'package:amazon_flutter/constants/global_variables.dart';
 import 'package:amazon_flutter/constants/utils.dart';
+import 'package:amazon_flutter/features/auth/screens/auth_screen.dart';
 import 'package:amazon_flutter/models/order.dart';
 import 'package:amazon_flutter/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AccountServices {
   Future<List<Order>> fetchMyOrders({required BuildContext context}) async {
@@ -24,12 +26,25 @@ class AccountServices {
           context: context,
           onSuccess: () {
             for (int i = 0; i < jsonDecode(res.body).length; i++) {
-              orderList.add(Order.fromJson(jsonEncode(jsonDecode(res.body)[i])));
+              orderList
+                  .add(Order.fromJson(jsonEncode(jsonDecode(res.body)[i])));
             }
           });
     } catch (e) {
       showSnackBar(context, e.toString());
     }
     return orderList;
+  }
+
+  void logOut({required BuildContext context}) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('-x-auth-token', '');
+      Navigator.pushNamedAndRemoveUntil(
+          context, AuthScreen.routeName, (route) => false);
+          
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
   }
 }
